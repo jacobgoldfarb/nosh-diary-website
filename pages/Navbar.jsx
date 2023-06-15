6cf046dcb52873360e46e2635965a502d3081e0d
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2023-06-15 13:10:59 -0400

    fix string literals

--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
       <ul className="flex space-x-4 justify-end p-10">
         <li>
           <Link href="/" legacyBehavior>
-            <a className="text-gray-600 hover:text-gray-900">Home</a>
+            <a className="text-gray-600 hover:text-gray-900">{"Home"}</a>
           </Link>
         </li>
         <li>
           <Link href="/About" legacyBehavior>
-            <a className="text-gray-600 hover:text-gray-900">About</a>
+            <a className="text-gray-600 hover:text-gray-900">{"About"}</a>
           </Link>
         </li>
       </ul>
--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ const Home = () => {
       <div className="container mx-auto py-10">
         <div className="flex justify-center items-center">
           <div className="flex flex-col max-w-md">
-            <h1 className="text-4xl font-bold mb-6">Welcome to Nosh Diary</h1>
-            <h2 className="text-2xl font-semibold mb-4">Unlock the Power of AI for Personalized Nutrition</h2>
+            <h1 className="text-4xl font-bold mb-6">{"Welcome Nosh Diary"}</h1>
+            <h2 className="text-2xl font-semibold mb-4">{"Unlock the Power of AI for Personalized Nutrition"}</h2>
             <p className="text-lg mb-6">
-              Redefine the way you nourish your body and mind with Nosh Diary's AI-powered nutrition app.
+              {"Redefine the way you nourish your body and mind with Nosh Diary, an AI-powered nutrition app."}
             </p>
             <p className="text-lg">
-              Experience the future of nutrition with our cutting-edge technology that combines artificial intelligence and personalized insights. Tailored to your unique needs and goals, our app provides you with customized nutrition plans, intelligent recommendations, and real-time tracking to help you achieve optimal health and well-being.
+              {"Experience the future of nutrition with our cutting-edge technology that combines artificial intelligence and personalized insights. Tailored to your unique needs and goals, our app provides you with customized nutrition plans, intelligent recommendations, and real-time tracking to help you achieve optimal health and well-being."}
             </p>
           </div>
           <div className="mx-auto rounded-lg w-96">
