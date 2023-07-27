3e80751f9c6d823e12c98ad9587ff00c7a073062
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2023-07-27 01:04:16 -0400

    made responsive

--- pages/index.js
+++ pages/index.js
@@ -8,21 +8,21 @@ import appStoreIcon from '../public/download_on_app_store.svg'; // import the SV
 const Home = () => {
   return (
     <Layout>
-      <div className="container mx-auto">
-        <div className="flex items-center">
-          <div className="flex flex-col max-w-lg">
-            <div className="text-8xl text-left font-bold text-tagline">
-                Discover self-gratification in your health through Nosh.
+      <div className="container px-4 mx-auto md:px-0">
+        <div className="flex flex-col-reverse md:flex-row items-center md:space-x-10">
+          <div className="flex flex-col items-center md:items-start md:w-1/2">
+            <div className="text-4xl md:text-tagline text-8xl text-center md:text-left font-bold mb-6">
+                Your health odyssey begins today. Try Nosh for free.
             </div>
-            <p className="text-xl mb-6 mt-16">
+            <p className="text-lg md:text-xl mb-6 text-center md:text-left">
             {"With Nosh you can easily keep an eye on what you eat. Just tell it what you're having or jot it down in the journal - it's as simple as that. The app will track your macros and use AI to give you insights based on your fitness goals. It's a practical, user-friendly tool that helps you manage your nutrition effectively. No fuss, just you moving closer to your health goals, one day at a time."}
             </p>
             <Link href={'https://www.apple.com/app-store/'}>
-              <Image src={appStoreIcon} alt="Download on App Store" width={200} height={60} /> {/* Render the SVG */}
+              <Image src={appStoreIcon} alt="Download on App Store" width={140} height={42} /> {/* Render the SVG */}
             </Link>
           </div>
-          <div className="rounded-lg w-100">
-            <Image src={appImage} alt="Nosh Diary App" width={1400} height={800} />
+          <div className="-mt-8 md:mt-0 w-full md:w-1/2">
+            <Image src={appImage} alt="Nosh Diary App" width={800} height={342}  />
           </div>
         </div>
       </div>
