b86fb9a7187834e6179a699020643efcc16cf1f4
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2023-07-27 12:08:21 -0400

    Formatting and cleaned magic strings

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,31 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import Image from 'next/image';
 import appImage from '../public/nosh-diary-mockup-clay.png';
-import appStoreIcon from '../public/download_on_app_store.svg'; // import the SVG
+import appStoreIcon from '../public/download_on_app_store.svg'; 
+
+// Text constants
+const TAGLINE = 'Your health odyssey begins today. Try Nosh for free.';
+const APP_DESCRIPTION = `With Nosh you can easily keep an eye on what you eat. Just tell it what you're having or jot it down in the journal - it's as simple as that. The app will track your macros and use AI to give you insights based on your fitness goals. It's a practical, user-friendly tool that helps you manage your nutrition effectively. No fuss, just you moving closer to your health goals, one day at a time.`;
+const APP_STORE_URL = 'https://www.apple.com/app-store/';
+const APP_STORE_ALT = 'Download on App Store';
 
 const Home = () => {
   return (
     <Layout>
       <div className="relative container mx-auto md:px-0">
-        <div className="flex flex-col md:flex-row-reverse items-center md:space-x-10 z-10">
-          <div className="w-full md:w-2/3">
-            <Image src={appImage} alt="Nosh Diary App" />
+        <div className="flex flex-col md:flex-row-reverse items-center md:space-x-20 z-10">
+          <div className="w-full md:w-1/2">
+            <Image src={appImage} className="md:-ml-32 md:min-w-max" alt="Nosh Diary App" />
           </div>
-          <div className="flex flex-col items-center md:items-start md:w-1/2 mt-10 md:mt-0">
+          <div className="flex flex-col items-center md:items-start md:w-1/2 mt-10 md:mt-0 ">
             <div className="text-5xl md:text-7xl text-center md:text-left font-bold mb-6">
-                Your health odyssey begins today. Try Nosh for free.
+                {TAGLINE}
             </div>
             <p className="text-lg md:text-xl mb-6 text-center md:text-left">
-            {"With Nosh you can easily keep an eye on what you eat. Just tell it what you're having or jot it down in the journal - it's as simple as that. The app will track your macros and use AI to give you insights based on your fitness goals. It's a practical, user-friendly tool that helps you manage your nutrition effectively. No fuss, just you moving closer to your health goals, one day at a time."}
+              {APP_DESCRIPTION}
             </p>
-            <Link href={'https://www.apple.com/app-store/'}>
-              <Image src={appStoreIcon} alt="Download on App Store" width={140} height={42} /> {/* Render the SVG */}
+            <Link href={APP_STORE_URL}>
+              <Image src={appStoreIcon} alt={APP_STORE_ALT} width={140} height={42} />
             </Link>
           </div>
         </div>
